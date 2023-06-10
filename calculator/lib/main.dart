2d845041f18c2a803f54f643b6ea89c2f66d72af
Apoorv yash <apoorvyash123@gmail.com>
2023-06-11 01:53:36 +0530

    added one piece layout

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(Calculator());
@@ -24,12 +25,17 @@ class CalculatorPage extends StatefulWidget {
 }
 
 class _CalculatorPageState extends State<CalculatorPage> {
+  String equation = '0';
+  String result = '0';
+  String expression = '';
+  double equationFontSize = 38.0;
+  double resultFontSize = 48.0;
   Widget buildButton(String text, double buttonHeight, Color buttonColor) {
     return Container(
       height: MediaQuery.of(context).size.height * buttonHeight,
       color: buttonColor,
       child: MaterialButton(
-        onPressed: null,
+        onPressed: () => buttonPressed(text),
         padding: const EdgeInsets.all(16.0),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(0.0),
@@ -49,98 +55,231 @@ class _CalculatorPageState extends State<CalculatorPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Nankau calculator'),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.centerRight,
-            padding: const EdgeInsets.fromLTRB(10, 10, 20, 0),
-            child: const Text(
-              '0',
-              style: TextStyle(fontSize: 38),
-            ),
+    return Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/one_piece.png"),
+            fit: BoxFit.cover,
           ),
-          Container(
-            alignment: Alignment.centerRight,
-            padding: const EdgeInsets.fromLTRB(10, 30, 20, 0),
-            child: const Text(
-              '0',
-              style: TextStyle(fontSize: 48),
-            ),
+        ),
+        child: Scaffold(
+          backgroundColor: Color.fromARGB(133, 255, 255, 255),
+          appBar: AppBar(
+            title: const Text("One piece calculator"),
+            centerTitle: true,
           ),
-          const Expanded(
-            child: Divider(),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+          body: Column(
             children: <Widget>[
               Container(
-                width: MediaQuery.of(context).size.width * 0.75,
-                child: Table(
-                  children: [
-                    TableRow(
-                      children: [
-                        buildButton('1', 0.1, Colors.black54),
-                        buildButton('2', 0.1, Colors.black54),
-                        buildButton('3', 0.1, Colors.black54),
-                      ],
-                    ),
-                    TableRow(
-                      children: [
-                        buildButton('4', 0.1, Colors.black54),
-                        buildButton('5', 0.1, Colors.black54),
-                        buildButton('6', 0.1, Colors.black54),
-                      ],
-                    ),
-                    TableRow(
-                      children: [
-                        buildButton('7', 0.1, Colors.black54),
-                        buildButton('8', 0.1, Colors.black54),
-                        buildButton('9', 0.1, Colors.black54),
-                      ],
-                    ),
-                    TableRow(
-                      children: [
-                        buildButton('.', 0.1, Colors.black54),
-                        buildButton('0', 0.1, Colors.black54),
-                        buildButton('00', 0.1, Colors.black54),
-                      ],
-                    ),
-                    TableRow(children: [
-                      buildButton('C', 0.15, Colors.blue),
-                      buildButton('⌫', 0.15, Colors.red),
-                      buildButton('÷', 0.15, Colors.blue),
-                    ]),
-                  ],
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/one_piece.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                alignment: Alignment.centerRight,
+                padding: const EdgeInsets.fromLTRB(10, 10, 20, 0),
+                child: Text(
+                  result,
+                  style: const TextStyle(fontSize: 38),
                 ),
               ),
               Container(
-                width: MediaQuery.of(context).size.width * 0.25,
-                child: Table(
-                  children: [
-                    TableRow(children: [
-                      buildButton('×', 0.1, Colors.blue),
-                    ]),
-                    TableRow(children: [
-                      buildButton('+', 0.1, Colors.blue),
-                    ]),
-                    TableRow(children: [
-                      buildButton('-', 0.1, Colors.blue),
-                    ]),
-                    TableRow(children: [
-                      buildButton('=', 0.25, Colors.green),
-                    ]),
-                  ],
+                alignment: Alignment.centerRight,
+                padding: const EdgeInsets.fromLTRB(10, 30, 20, 0),
+                child: Text(
+                  equation,
+                  style: const TextStyle(fontSize: 48),
                 ),
+              ),
+              const Expanded(
+                child: Divider(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.75,
+                    child: Table(
+                      children: [
+                        TableRow(
+                          children: [
+                            buildButton('1', 0.1, Colors.black54),
+                            buildButton('2', 0.1, Colors.black54),
+                            buildButton('3', 0.1, Colors.black54),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            buildButton('4', 0.1, Colors.black54),
+                            buildButton('5', 0.1, Colors.black54),
+                            buildButton('6', 0.1, Colors.black54),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            buildButton('7', 0.1, Colors.black54),
+                            buildButton('8', 0.1, Colors.black54),
+                            buildButton('9', 0.1, Colors.black54),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            buildButton('.', 0.1, Colors.black54),
+                            buildButton('0', 0.1, Colors.black54),
+                            buildButton('00', 0.1, Colors.black54),
+                          ],
+                        ),
+                        TableRow(children: [
+                          buildButton(
+                              'C', 0.15, Color.fromARGB(230, 65, 170, 255)),
+                          buildButton(
+                              '⌫', 0.15, const Color.fromRGBO(244, 67, 54, 40)),
+                          buildButton(
+                              '÷', 0.15, Color.fromARGB(230, 65, 170, 255)),
+                        ]),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.25,
+                    child: Table(
+                      children: [
+                        TableRow(children: [
+                          buildButton(
+                              '×', 0.1, Color.fromARGB(230, 65, 170, 255)),
+                        ]),
+                        TableRow(children: [
+                          buildButton(
+                              '+', 0.1, Color.fromARGB(230, 65, 170, 255)),
+                        ]),
+                        TableRow(children: [
+                          buildButton(
+                              '-', 0.1, Color.fromARGB(230, 65, 170, 255)),
+                        ]),
+                        TableRow(children: [
+                          buildButton(
+                              '=', 0.25, Color.fromRGBO(76, 175, 80, 30)),
+                        ]),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
-      ),
-    );
+        ));
+  }
+
+  buttonPressed(String text) {
+    if (text == 'C') {
+      setState(() {
+        equation = '0';
+        result = '';
+        expression = '';
+      });
+    }
+    if (text == '⌫') {
+      setState(() {
+        equation = equation.substring(0, equation.length - 1);
+        if (equation == '') {
+          equation = '0';
+        }
+      });
+    }
+    if (text == '=') {
+      setState(() {
+        expression = equation;
+        expression = expression.replaceAll('×', '*');
+        expression = expression.replaceAll('÷', '/');
+        try {
+          Parser p = Parser();
+          Expression exp = p.parse(expression);
+          ContextModel cm = ContextModel();
+          result = '${exp.evaluate(EvaluationType.REAL, cm)}';
+        } catch (e) {
+          result = 'Error';
+        }
+      });
+    }
+
+    if (text == '÷') {
+      if (equation == '0') {
+        setState(() {
+          equation = text;
+        });
+      } else {
+        setState(() {
+          equation += text;
+        });
+      }
+    }
+    if (text == '×') {
+      if (equation == '0') {
+        setState(() {
+          equation = text;
+        });
+      } else {
+        setState(() {
+          equation += text;
+        });
+      }
+    }
+
+    if (text == '+') {
+      if (equation == '0') {
+        setState(() {
+          equation = text;
+        });
+      } else {
+        setState(() {
+          equation += text;
+        });
+      }
+    }
+    if (text == '-') {
+      if (equation == '0') {
+        setState(() {
+          equation = text;
+        });
+      } else {
+        setState(() {
+          equation += text;
+        });
+      }
+    }
+
+    if (text == '.') {
+      if (equation == '0') {
+        setState(() {
+          equation = text;
+        });
+      } else {
+        setState(() {
+          equation += text;
+        });
+      }
+    }
+    if (text == '00' ||
+        text == '0' ||
+        text == '1' ||
+        text == '2' ||
+        text == '3' ||
+        text == '4' ||
+        text == '5' ||
+        text == '6' ||
+        text == '7' ||
+        text == '8' ||
+        text == '9') {
+      if (equation == '0') {
+        setState(() {
+          equation = text;
+        });
+      } else {
+        setState(() {
+          equation += text;
+        });
+      }
+    }
   }
 }
